be7dd7cebf1b20a6f741529158caa8e20961f5af
DucNguyen <ducnguyen412k2@gmail.com>
2023-03-22 21:18:43 +0700

    Duc

--- app/src/main/java/com/example/healthcare/DoctorDetailsActivity.java
+++ app/src/main/java/com/example/healthcare/DoctorDetailsActivity.java
@@ -16,44 +16,44 @@ import java.util.HashMap;
 
 public class DoctorDetailsActivity extends AppCompatActivity {
     private String[][] doctors_details1 ={
-            {"Doctor Name:Dr. John With","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. Jane Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. John Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. Jane Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. John Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. Jane Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"}
-            };
+            {"Doctor Name:Dr. Hoàng Gia Du","Hospital Address: Đống Đa, Hà Nội","Exp :10 năm","Mobile Number: 0382108926","600"},
+            {"Doctor Name:Dr. Đào Xuân Thành","Hospital Address: Đông Anh, Hà Nôi","Exp :8 năm","Mobile Number: 0967094928","500"},
+            {"Doctor Name:Dr. Nguyễn Thế Hào","Hospital Address: Thanh Xuân, Hà Nội","Exp :9 năm","Mobile Number:  090443003","400"},
+            {"Doctor Name:Dr. Vũ Thế Xuân","Hospital Address: Long Biên, Hà Nội","Exp :12 năm","Mobile Number: 0932781567","700"},
+            {"Doctor Name:Dr. Chử Xuân Tùng","Hospital Address: Gia Lâm, Hà Nội","Exp :6 năm","Mobile Number: 0978729742","450"},
+            {"Doctor Name:Dr. Phùng Xuân Nhạn","Hospital Address: Hoàn Kiếm, Hà Nội  ","Exp :11 năm","Mobile Number: 0967224743","600"}
+    };
     private String[][] doctors_details2 ={
-            {"Doctor Name:Dr. John Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. Jane Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. John Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. Jane Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. John Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. Jane Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"}
+            {"Doctor Name:Dr. Nguyễn Xuân Hùng","Hospital Address: Đông Anh, Hà Nôi","Exp :8 năm","Mobile Number: 0967094928","500"},
+            {"Doctor Name:Dr. Phạm Hoàng Hà","Hospital Address: Gia Lâm, Hà Nội","Exp :6 năm","Mobile Number: 0978729742","450"},
+            {"Doctor Name:Dr. Đỗ Mai Lâm","Hospital Address: Đống Đa, Hà Nội","Exp :6 năm","Mobile Number: 0978729742","450"},
+            {"Doctor Name:Dr. Tống Quang Hiếu","Hospital Address: Thanh Xuân, Hà Nội","Exp :9 năm","Mobile Number:  090443003","400"},
+            {"Doctor Name:Dr. Vũ Đức Thịnh","Hospital Address: Long Biên, Hà Nội","Exp :12 năm","Mobile Number: 0932781567","700"},
+            {"Doctor Name:Dr. Phạm Hiếu Tâm","Hospital Address: Hoàn Kiếm, Hà Nội  ","Exp :11 năm","Mobile Number: 0967224743","600"}
     };
     private String[][] doctors_details3 ={
-            {"Doctor Name:Dr. John Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. Jane Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. John Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. Jane Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. John Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. Jane Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"}
+            {"Doctor Name:Dr. Nguyễn Trung Đức","Hospital Address: Hà Đông, Hà Nội","Exp :11 năm","Mobile Number: 0333224321","300"},
+            {"Doctor Name:Dr. Phạm Minh Tuấn","Hospital Address: Sơn Tây, Hà Nội","Exp :10 năm","Mobile Number: 012322234","400"},
+            {"Doctor Name:Dr. Lưu Văn Vũ","Hospital Address: Từ Sơn, Bắc Ninh","Exp :7 năm","Mobile Number: 0987678984","500"},
+            {"Doctor Name:Dr. Lê Phú Minh","Hospital Address: Long Biên, Hà Nội","Exp :8 năm","Mobile Number: 0987766455","200"},
+            {"Doctor Name:Dr. Ngô Văn Anh","Hospital Address: Gia Lâm, Hà Nội","Exp :15 năm","Mobile Number: 0123123123","450"},
+            {"Doctor Name:Dr. Vương Kiến Quốc","Hospital Address: Sóc Sơn, Hà Nội","Exp :13 năm","Mobile Number: 0987232123","700"}
     };
     private String[][] doctors_details4 ={
-            {"Doctor Name:Dr. John Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. Jane Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. John Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. Jane Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. John Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. Jane Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"}
+            {"Doctor Name:Dr. Nguyễn Xuân Hùng","Hospital Address: Đông Anh, Hà Nôi","Exp :8 năm","Mobile Number: 0967094928","500"},
+            {"Doctor Name:Dr. Phạm Hoàng Hà","Hospital Address: Gia Lâm, Hà Nội","Exp :6 năm","Mobile Number: 0978729742","450"},
+            {"Doctor Name:Dr. Đỗ Mai Lâm","Hospital Address: Đống Đa, Hà Nội","Exp :6 năm","Mobile Number: 0978729742","450"},
+            {"Doctor Name:Dr. Tống Quang Hiếu","Hospital Address: Thanh Xuân, Hà Nội","Exp :9 năm","Mobile Number:  090443003","400"},
+            {"Doctor Name:Dr. Vũ Đức Thịnh","Hospital Address: Long Biên, Hà Nội","Exp :12 năm","Mobile Number: 0932781567","700"},
+            {"Doctor Name:Dr. Phạm Hiếu Tâm","Hospital Address: Hoàn Kiếm, Hà Nội  ","Exp :11 năm","Mobile Number: 0967224743","600"}
     };
     private String[][] doctors_details5 ={
-            {"Doctor Name:Dr. John Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. Jane Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. John Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. Jane Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. John Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"},
-            {"Doctor Name:Dr. Jane Doe","Hospital Address: 1234 Main Street, New York","Exp :10 years","Mobile Number: 1234567890","600"}
+            {"Doctor Name:Dr. Hoàng Gia Du","Hospital Address: Đống Đa, Hà Nội","Exp :10 năm","Mobile Number: 0382108926","600"},
+            {"Doctor Name:Dr. Đào Xuân Thành","Hospital Address: Đông Anh, Hà Nôi","Exp :8 năm","Mobile Number: 0967094928","500"},
+            {"Doctor Name:Dr. Nguyễn Thế Hào","Hospital Address: Thanh Xuân, Hà Nội","Exp :9 năm","Mobile Number:  090443003","400"},
+            {"Doctor Name:Dr. Vũ Thế Xuân","Hospital Address: Long Biên, Hà Nội","Exp :12 năm","Mobile Number: 0932781567","700"},
+            {"Doctor Name:Dr. Chử Xuân Tùng","Hospital Address: Gia Lâm, Hà Nội","Exp :6 năm","Mobile Number: 0978729742","450"},
+            {"Doctor Name:Dr. Phùng Xuân Nhạn","Hospital Address: Hoàn Kiếm, Hà Nội  ","Exp :11 năm","Mobile Number: 0967224743","600"}
     };
     TextView txtTitle;
     Button btnBack;
