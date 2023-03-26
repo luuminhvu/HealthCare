5f9e2a790581fb6560dcdedd646ab5fa0c84a819
Luu Vu <vu7178@gmail.com>
2023-03-26 21:15:45 +0700

    Finally

--- app/src/main/java/com/example/healthcare/InformationAppActivity.java
+++ app/src/main/java/com/example/healthcare/InformationAppActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,6 +14,7 @@ public class InformationAppActivity extends AppCompatActivity {
     Button btnBack;
     ImageView img;
     TextView tv;
+    EditText tvInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,8 @@ public class InformationAppActivity extends AppCompatActivity {
         btnBack = findViewById(R.id.buttonInfoAppBack);
         img = findViewById(R.id.imgViewContact);
         tv = findViewById(R.id.tvEmailContact);
+        tvInfo = findViewById(R.id.edTextInfoApp);
+        tvInfo.setKeyListener(null);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
